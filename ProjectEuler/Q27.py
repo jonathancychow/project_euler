e8a30dadb7ba98474d9e63652db254dd2ea76809
Jonathan Chow <jonathan.chow@mclaren.com>
2019-02-10 22:26:39 +0000

    Updated Q27.py

--- ProjectEuler/Q27.py
+++ ProjectEuler/Q27.py
@@ -1,4 +1,4 @@
-import time, numpy, sys, os
+import time, sys, os
 
 ThisFolder = os.path.dirname(os.path.abspath(__file__))
 PathsToAdd = [os.path.abspath(ThisFolder),
@@ -10,16 +10,19 @@ for PathToAdd in PathsToAdd:
 
 from PrimeLib import *
 
-limit = 1603
+# Define the upper and the lower limit of the check
+limit = 1000
+# Initial values
 n = 0
 max_prime = 0
 max_i = 0
 max_j = 0
 quartic_sum = 2
 tstart = time.time()
+
 for i in range (-limit, limit,1):
     for j in range (-limit, limit,1):
-        while isPrime2(quartic_sum)==True & bool(quartic_sum > 0)==True :
+        while isPrime2(quartic_sum)==True:
             quartic_sum = n**2 + i * n + j
             n += 1
 
@@ -28,12 +31,13 @@ for i in range (-limit, limit,1):
             max_i = i
             max_j = j
 
-        n = 0
+        # reset n and quartic_sum to initial value
         quartic_sum = 2
+        n = 0
 
 
 print (max_j)
 print (max_i)
-print (max_j* max_i)
+print ('Answer = ',max_j* max_i)
 tend = time.time()
 print('Run time for Q27 =',tend-tstart,'s')
