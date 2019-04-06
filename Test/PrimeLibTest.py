eb9ae043db3ac6e53bbc02704b584dde586834a6
Jonathan Chow <jonathan.chow@mclaren.com>
2019-04-06 16:15:36 +0100

    Converted array to numpy array

--- Test/PrimeLibTest.py
+++ Test/PrimeLibTest.py
@@ -1,5 +1,5 @@
 import sys, os, time, glob
-
+import numpy as np
 ThisFolder = os.path.dirname(os.path.abspath(__file__))
 PathsToAdd = [os.path.abspath(ThisFolder),
               os.path.abspath(os.path.join(ThisFolder,'..'))]
@@ -10,28 +10,19 @@ for PathToAdd in PathsToAdd:
 
 from PrimeLib import *
 
-# prime_text_file = "".join(('Track', '\\', Track, '.csv' ))
-# prime_text_file = 'primes-to-100k.txt'
-prime_list=[]
 text_file = glob.glob("*.txt")
+prime_list = np.array([])
 for i in range(len(text_file)):
     prime_text_file = text_file[i]
     with open(prime_text_file, encoding='utf-8-sig') as f:
-        # prime_list = f.read().splitlines()
-        prime_list.append(f.read().splitlines())
-
-# Flatten prime list, better convert to np array at a later stage
-flat_prime_list = []
-for sublist in prime_list:
-    for item in sublist:
-        flat_prime_list.append(item)
+        prime_list = np.append(prime_list,f.read().splitlines())
 
 # Convert list of string to list of int
-flat_prime_list = list(map(int, flat_prime_list))
+prime_list = list(map(int, prime_list))
 
 Pass = []
 tstart = time.time()
-for i in flat_prime_list:
+for i in prime_list:
     Pass.append(isPrime2(i))
 
 if len(Pass) == sum(Pass):
