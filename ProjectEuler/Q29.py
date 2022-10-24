664a8c7d07bc873469c90ccc068f9c9b7d4003e4
Jonathan Chow <jonathan@cychow.co.uk>
2022-10-24 12:59:33 +0100

    Refactored Q29

--- ProjectEuler/Q29.py
+++ ProjectEuler/Q29.py
@@ -6,9 +6,7 @@ a = list(range(2,n))
 b = list(range(2,n))
 
 combination = list(permutations(a+b, 2))
-number_from_combination=[]
-for i in range(len(combination)):
-    number_from_combination.append(combination[i][0]**combination[i][1])
+number_from_combination = [comb[0]**comb[1] for comb in combination ]
 
 distinct_numbers = set(number_from_combination)
 print('Answer of Q29 = ',len(distinct_numbers))
